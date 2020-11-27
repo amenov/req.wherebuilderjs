e610f5243c97c72b7d42c9dd611e29ba826245b9
Абдулсалам Аменов <amenov.abdulsalam@gmail.com>
2020-11-27 12:16:32 +0600

    M 1216 27112020

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const WhereBuilder = require('amenov.wherebuilderjs');
 
-module.exports = (req, res, next) => {
+module.exports = () => (req, res, next) => {
   req.wherebuilder = async (target, raw) => {
     let request = {};
 
